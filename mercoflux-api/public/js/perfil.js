9512ee833953469bc7d1eaab3e5a1822a3c61b03
AgdaTaniguchi <agda.taniguchi@sptech.school>
2022-05-31 13:29:13 -0300

    Alteração perfil

--- mercoflux-api/public/js/perfil.js
+++ mercoflux-api/public/js/perfil.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     preencherDadosPessoais();
     listarUsuarios();
-    // preencherDadosMercado();
+    preencherDadosMercado();
     // listarMudancas();
 }
 
@@ -37,20 +37,43 @@ function listarUsuarios(){
                 // console.log(resposta);
 
                 for (let contador = 0; contador < resposta.length; contador++) {
-                    tableUsuarios.innerHTML += `<tr>
-                        <td>${resposta[contador].nome}</td>
-                        <td>${resposta[contador].email}</td>`;
+                    let tr = "";
+                    tr += `<tr><td>${resposta[contador].nome}</td><td>${resposta[contador].email}</td>`;
 
-                    if (resposta[contador].administradorPrincial == 1) {
-                        tableUsuarios.innerHTML += `<td><button><a href=#Dados_P>Alterar usuário</button></td>`;
+                    if (resposta[contador].administradorPrincipal == 1) {
+                        tr += `<td><button><a href=#Dados_P style="color: white;">Alterar usuário</button></td>`;
                     }
                     else {
-                        tableUsuarios.innerHTML += `<td><button onclick="removerUsuario(${resposta[contador].idRepresentante})">Remover usuário</button></td>`;
+                        tr += `<td><button onclick="removerUsuario(${resposta[contador].idRepresentante})">Remover usuário</button></td>`;
                     }
 
-                    tableUsuarios.innerHTML += `</tr>`;
+                    tr += `</tr>`;
+                    tableUsuarios.innerHTML += tr;
                 }
                 
+            })
+        }
+        else{
+            console.error("Nenhum dado encontrado ou erro na API");
+        }
+    })
+    .catch((erro) => {
+        console.error(`Erro na obtenção dos dados - Dados usuário: ${erro.message}`);
+    });
+}
+
+function preencherDadosMercado(){
+    fetch(`usuarios/pegarDadosPessoais?idUsuario=${sessionStorage.ID_USUARIO}`)
+    .then((res) => {
+        if(res.ok){
+            res.json()
+            .then((resposta) => {
+                // console.log(resposta);
+
+                nomeUsuarioAdministrador.value = resposta.nome;
+                emailUsuarioAdministrador.value = resposta.email;
+                senhaUsuarioAdministrador.value = resposta.senha;
+                cpfUsuarioAdministrador.value = resposta.cpf;
             })
         }
         else{
